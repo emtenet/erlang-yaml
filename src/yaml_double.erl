f183c2513d95d6c0fd27827df615efee5bb2098a
emtenet <michael@emte.net.au>
2018-11-03 11:52:28 +1100

    Example 7.5 Double Quoted Line Breaks NOTE: Output non-content breaks in folds the same as for an escaped break

--- src/yaml_double.erl
+++ src/yaml_double.erl
@@ -34,11 +34,12 @@ construct(Ps, #{ from := From, thru := Thru, anchor := Anchor, tag := Tag}) ->
 
 %-----------------------------------------------------------------------
 
-fold_by_space([{F, T, fold}, Text = {_, _, B} | Rest], Acc)
-        when is_binary(B) ->
-    fold_by_space(Rest, [Text, {F, T, <<" ">>} | Acc]);
-fold_by_space(Rest = [{_, _, fold} | _], Acc) ->
+fold_by_space([{_, _, escape} | Rest], Acc) ->
+    fold_by_space(Rest, Acc);
+fold_by_space(Rest = [{_, _, fold}, {_, _, fold} | _], Acc) ->
     fold_by_line(Rest, Acc);
+fold_by_space([{F, T, fold} | Rest], Acc) ->
+    fold_by_space(Rest, [{F, T, <<" ">>} | Acc]);
 fold_by_space([Text | Rest], Acc) ->
     fold_by_space(Rest, [Text | Acc]);
 fold_by_space([], Acc) ->
@@ -46,11 +47,10 @@ fold_by_space([], Acc) ->
 
 %-----------------------------------------------------------------------
 
-fold_by_line([{_, T, fold}, {F, _, fold}, Text = {_, _, B} | Rest], Acc)
-        when is_binary(B) ->
-    fold_by_space(Rest, [Text, {F, T, <<"\n">>} | Acc]);
-fold_by_line([{F, T, fold} | Rest], Acc) ->
-    fold_by_line(Rest, [{F, T, <<"\n">>} | Acc]).
+fold_by_line([{F, T, fold} | Rest = [{_, _, fold} | _]], Acc) ->
+    fold_by_line(Rest, [{F, T, <<"\n">>} | Acc]);
+fold_by_line([{_, _, fold} | Rest], Acc) ->
+    fold_by_space(Rest, Acc).
 
 %=======================================================================
 
@@ -64,11 +64,14 @@ first(Style, T, S) ->
 text(Style, T, S) ->
     case yaml_scan:grapheme(S) of
         break ->
-            fold(Style, T, S, yaml_scan:next(S));
+            fold(Style, fold, T, S, yaml_scan:next(S));
 
         $\" ->
             text_finish(Style, T, S);
             
+        $\\ ->
+            text_escape(Style, T, S, yaml_scan:next(S));
+
         G when ?IS_WHITE(G) ->
             text_white(Style, T, S, yaml_scan:next(S));
 
@@ -81,11 +84,14 @@ text(Style, T, S) ->
 text_white(Style, T, White, S) ->
     case yaml_scan:grapheme(S) of
         break ->
-            fold(Style, T, White, yaml_scan:next(S));
+            fold(Style, fold, T, White, yaml_scan:next(S));
 
         $\" ->
             text_finish(Style, T, S);
             
+        $\\ ->
+            text_escape(Style, T, S, yaml_scan:next(S));
+
         G when ?IS_WHITE(G) ->
             text_white(Style, T, White, yaml_scan:next(S));
 
@@ -95,7 +101,21 @@ text_white(Style, T, White, S) ->
 
 %-----------------------------------------------------------------------
 
-text_finish(Style, T, S) ->
+text_escape(Style, T, Escape, S) ->
+    case yaml_scan:grapheme(S) of
+        break ->
+            fold(Style, escape, T, Escape, yaml_scan:next(S));
+
+        G when G =:= $\s orelse G =:= $\t ->
+            Z = yaml_scan:next(S),
+            Kept = yaml_token:keep(T, Escape),
+            Skip = yaml_token:skip(Kept, S),
+            text(Style, Skip, Z)
+    end.
+
+%-----------------------------------------------------------------------
+
+text_finish(_Style, T, S) ->
     Z = yaml_scan:next(S),
     Kept = yaml_token:keep(T, S),
     Skip = yaml_token:skip(Kept, Z),
@@ -103,9 +123,9 @@ text_finish(Style, T, S) ->
 
 %=======================================================================
 
-fold(Style, T, White, S) ->
+fold(Style, Fold, T, White, S) ->
     Abort = {T, White},
-    fold_break(Style, Abort, yaml_token:keep(T, White), White, S).
+    fold_break(Style, Abort, Fold, yaml_token:keep(T, White), White, S).
 
 %-----------------------------------------------------------------------
 
@@ -114,37 +134,37 @@ fold_abort({T, White}) ->
 
 %-----------------------------------------------------------------------
 
-fold_break(Style, Abort, T, White, S) ->
+fold_break(Style, Abort, Fold, T, White, S) ->
     case yaml_scan:end_of(S) of
         {_, _, _} ->
             fold_abort(Abort);
 
         false ->
-            fold_indent(Style, Abort, T, White, S)
+            fold_indent(Style, Abort, Fold, T, White, S)
     end.
 
 %-----------------------------------------------------------------------
 
-fold_indent(Style, Abort, T, White, S) ->
+fold_indent(Style, Abort, Fold, T, White, S) ->
     case yaml_scan:grapheme(S) of
         end_of_stream ->
             fold_abort(Abort);
 
         break ->
-            Kept = yaml_token:keep(T, fold, S),
-            fold_break(Style, Abort, Kept, S, yaml_scan:next(S));
+            Kept = yaml_token:keep(T, Fold, S),
+            fold_break(Style, Abort, fold, Kept, S, yaml_scan:next(S));
 
         $\s ->
-            fold_indent(Style, Abort, T, White, yaml_scan:next(S));
+            fold_indent(Style, Abort, Fold, T, White, yaml_scan:next(S));
 
         $\t ->
             Indented = yaml_token:is_indented(T, S),
-            fold_white(Style, Abort, T, White, Indented, yaml_scan:next(S));
+            fold_white(Style, Abort, Fold, T, White, Indented, yaml_scan:next(S));
 
         G when ?IS_PRINTABLE(G) ->
             case yaml_token:is_indented(T, S) of
                 true ->
-                    Kept = yaml_token:keep(T, fold, S),
+                    Kept = yaml_token:keep(T, Fold, S),
                     text(Style, Kept, S);
 
                 false ->
@@ -154,22 +174,22 @@ fold_indent(Style, Abort, T, White, S) ->
 
 %-----------------------------------------------------------------------
 
-fold_white(Style, Abort, T, White, Indented, S) ->
+fold_white(Style, Abort, Fold, T, White, Indented, S) ->
     case yaml_scan:grapheme(S) of
         end_of_stream ->
             fold_abort(Abort);
 
         break ->
-            Kept = yaml_token:keep(T, fold, S),
-            fold_break(Style, Abort, Kept, S, yaml_scan:next(S));
+            Kept = yaml_token:keep(T, Fold, S),
+            fold_break(Style, Abort, fold, Kept, S, yaml_scan:next(S));
 
         G when ?IS_WHITE(G) ->
-            fold_white(Style, Abort, T, White, Indented, yaml_scan:next(S));
+            fold_white(Style, Abort, Fold, T, White, Indented, yaml_scan:next(S));
 
         G when ?IS_PRINTABLE(G) ->
             case Indented of
                 true ->
-                    Kept = yaml_token:keep(T, fold, S),
+                    Kept = yaml_token:keep(T, Fold, S),
                     text(Style, Kept, S);
 
                 false ->
